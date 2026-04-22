d1e46da1a4db8017ab1165200830f8bc727de649
Hafizh-ajja <hhafizh662@email.com>
2026-04-22 15:19:45 +0700

    Process

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ const hideMenu = () => {
 
 closeMenu.addEventListener('click', hideMenu);
 Overlay.addEventListener('click', hideMenu);
+
+function toggleFaq(element) {
+    const allItems = document.querySelectorAll("faq-items");
+    const content = element.querySelector("faq-content");
+    const img = element.querySelector("img");
+    const openIcon = "/asset/+ logo.svg";
+    const closeIcon = "/asset/- icon.svg";
+    const isOpening = !content.style.maxHeight;
+
+    allItems.forEach(item => {
+        const itemContent = item.querySelector('faq-content');
+        const itemImg = item.querySelector('img');
+        itemContent.style.maxHeight = null;
+        itemImg.src = openIcon
+    })
+
+
+    if (isOpening) {
+        content.style.maxHeight = content.scrollHeight + "px";
+        img.src = closeIcon
+    }
+};
